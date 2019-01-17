f778a84351860acf3894e43eeffeb78b2d6a2752
Brandon Stokes <bstokes@pivotal.io>
2019-01-16 18:50:07 -0700

    update zuul  + remove security  + update dependencies to allow pcf service bindings  + create pcf manifest

--- components/zuul/src/main/java/io/template/zuulrekaconfig/ZuulApplication.java
+++ components/zuul/src/main/java/io/template/zuulrekaconfig/ZuulApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class ZuulApplication {
+public class ZuulApplication extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception{
+    http.authorizeRequests().anyRequest().permitAll();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ZuulApplication.class, args);
